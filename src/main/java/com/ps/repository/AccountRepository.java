962d9aee59d40b23cf1aabd0ae75fab0ff461de1
fan9704 <cxz123499@gmail.com>
2022-09-15 00:30:30 +0800

    Redefine findByUsername method without @Query

--- src/main/java/com/ps/repository/AccountRepository.java
+++ src/main/java/com/ps/repository/AccountRepository.java
@@ -16,8 +16,7 @@ import java.util.List;
 @Repository
 public interface AccountRepository extends JpaRepository<Account, String> {
 
-    @Query("SELECT a FROM Account a WHERE a.username = :username")
-    Account findByUsername(@Param("username") String username);
+    Account findByUsername(String username);
 
     @Query("SELECT r FROM Account a, AccountRole ar, Role r WHERE a.username = :username and a.accountid = ar.accountid and ar.roleid = r.roleid")
     List<Role> findRoleListByUsername(@Param("username") String username);
